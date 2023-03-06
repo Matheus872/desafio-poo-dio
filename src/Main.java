1b4b07cb35185c321ec92fcee675c5c312e7444c
Matheus Henrique <48736650+Matheus872@users.noreply.github.com>
2023-03-06 20:35:17 -0300

    Add Mentoria and Cursos

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Mentoria;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("Desafio POO");
+        curso.setDescricao("Descrição do curso");
+        curso.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso.setTitulo("Curso Js");
+        curso.setDescricao("Descrição do curso");
+        curso.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição da Mentoria");
+        mentoria.setCargaHoraria(2);
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+
+    }
+
+}
---
+++ src/br/com/dio/desafio/domain/Mentoria.java
@@ -0,0 +1,42 @@
+package br.com.dio.desafio.domain;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    String titulo;
+    String descricao;
+    int cargaHoraria;
+    LocalDate data;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
